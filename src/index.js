e8d98251ae8d71523e0829311ea5ee7daad0201e
TarunNahak <tarunnahak25@gmail.com>
2022-11-06 06:17:36 +0000

    added routes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Battle from "../src/components/Battle";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 import "./styles.css";
 root.render(
-  <StrictMode>
-    {/* <App /> */}
-    <div className="light">
-      <div className="container">
-        {/* <Battle /> */}
-        <App />
+  <Router>
+    <StrictMode>
+      {/* <App /> */}
+      <div className="light">
+        <div className="container">
+          {/* <Battle /> */}
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/battle" element={<Battle />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-  </StrictMode>
+    </StrictMode>
+  </Router>
 );
